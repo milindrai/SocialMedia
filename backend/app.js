cd3611b15ace158e3e32cdd331ca30704395885c
milindrai <milindrai365@gmail.com>
2024-06-30 20:37:42 +0530

    Add server startup code

---
+++ backend/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();   
+const cookieParser = require('cookie-parser');
+
+require('dotenv').config({ path: 'backend/config/config.env' });
+
+
+
+//importing middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+//importing routes
+const post=require('./routes/post');
+const user=require('./routes/user');
+
+//using routes
+app.use('/api/v1', post);
+app.use('/api/v1', user);
+
+module.exports = app;
