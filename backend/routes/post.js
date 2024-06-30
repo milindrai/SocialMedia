65ed4f41e879f1677d8fa049d83e62b1b5618c65
milindrai <milindrai365@gmail.com>
2024-06-30 23:52:20 +0530

    Add route for uploading posts with authentication

---
+++ backend/routes/post.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const createPost = require('../controllers/post');
+const { isAuthenticated } = require('../middlewares/auth');
+
+
+const router = express.Router();
+
+router.route("/post/upload").post(isAuthenticated,createPost);
+
+
+module.exports = router;
