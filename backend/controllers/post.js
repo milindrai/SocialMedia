4b810146213101047e1a24b954d1645a482a1a5f
milindrai <milindrai365@gmail.com>
2024-07-01 08:28:43 +0530

    Add getPostOfFollowing endpoint

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -129,3 +129,26 @@ exports.commentOnPost = async (req, res) => {
         });
     }
 }
+
+
+exports.getPostOfFollowing = async (req, res) => {
+    try {
+        const user=await User.findById(req.user._id);
+        const posts=await Post.find({
+            owner: {
+                $in: user.following,
+            }
+        });
+        res.status(200).json({
+            success: true,
+            posts,
+        });
+    } 
+    
+    catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+}
--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createPost,deletePost ,likeAndUnlikePost,commentOnPost} = require('../controllers/post');
+const { createPost,deletePost ,likeAndUnlikePost,commentOnPost,getPostOfFollowing} = require('../controllers/post');
 const { isAuthenticated } = require('../middlewares/auth');
 
 const router = express.Router();
@@ -8,5 +8,6 @@ router.route("/post/upload").post(isAuthenticated, createPost);
 router.route("/post/:id").delete(isAuthenticated, deletePost);
 router.route("/post/:id").get(isAuthenticated, likeAndUnlikePost);
 router.route("/post/comment/:id").put(isAuthenticated, commentOnPost);
+router.route("/posts").get(isAuthenticated, getPostOfFollowing);
 
 module.exports = router;
