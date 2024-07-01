ba4cdc89d991f6b757b9cbea350e4af50d6d0740
milindrai <milindrai365@gmail.com>
2024-07-01 07:39:21 +0530

    Add logout route and controller function

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -85,3 +85,16 @@ exports.login = async (req, res) => {
         });
     }
 }
+
+
+exports.logout = async (req, res) => {
+    res.cookie('token', null, {
+        expires: new Date(Date.now()),
+        httpOnly: true,
+    });
+
+    res.status(200).json({
+        success: true,
+        message: 'Logged out',
+    });
+}
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { register, login } = require("../controllers/user");
+const { register, login,logout } = require("../controllers/user");
 const { isAuthenticated } = require("../middlewares/auth");
 
 
 router.route("/register").post(register);
 router.route("/login").post(login);
+router.route("/logout").post(isAuthenticated, logout);
 
 module.exports = router;
