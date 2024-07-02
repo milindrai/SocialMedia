1543b7b3629d6d9f453f47b7460585e894e65ecd
milindrai <milindrai365@gmail.com>
2024-07-02 18:08:12 +0530

    react setup

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { BrowserRouter as Router} from'react-router-dom';
+
+function App() {
+    return (
+      <Router>
+        <div className="App">
+          <h1>Hello World!</h1>
+        </div>
+      </Router>
+    );
+}
+
+export default App;
