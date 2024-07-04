c48369d3974e9228e6219f4ce65ceb6836c4f5c0
milindrai <milindrai365@gmail.com>
2024-07-04 20:03:45 +0530

    Update likeAndUnlikePost endpoint to use POST method

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.route("/post/upload").post(isAuthenticated, createPost);
 router.route("/post/:id").delete(isAuthenticated, deletePost);
-router.route("/post/:id").get(isAuthenticated, likeAndUnlikePost);
+router.route("/post/:id").post(isAuthenticated, likeAndUnlikePost);
 router.route("/post/comment/:id").post(isAuthenticated, commentOnPost);
 router.route("/posts").get(isAuthenticated, getPostOfFollowing);
 router.route("/post/:id").put(isAuthenticated, updateCaption);
